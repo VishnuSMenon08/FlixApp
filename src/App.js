7ea3645e74cb17c776ac1db17127b1e4995cb176
Vishnu S Menon <vishnusmenon08@gmail.com>
2022-10-06 12:51:30 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,22 +6,23 @@ import './App.css';
 function App() {
 
   const [data,setData] = useState([])
+  const [defaultFilter,setDefaultFilter] = useState({value : 5000})
   const fetchData =  async () => {
   const response = await fetch('https://api.flixpremiere.com/v1/films/filter/now_showing?limit=10', {
       method: 'GET',
     })
   const newdata = await response.json()
-  console.log(newdata.films)
   setData(newdata.films)
+  setDefaultFilter({value:5500})
 
   }
 
   useEffect(()=>{
     fetchData()
-  },[])
+  },5000)
   return (
     <div className="App">
-      <Card filmsData={data}/>
+      <Card filmsData={data} timeFilter={defaultFilter}/>
     </div>
   );
 }
