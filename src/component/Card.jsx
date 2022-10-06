7d2c7d439c4282ab4fb072bc255fc9e0ed584adf
Vishnu S Menon <vishnusmenon08@gmail.com>
2022-10-06 12:52:13 +0100

    Update Card.jsx

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import  './Card.css';
 
-const Card =  (filmsData) =>{
-    filmsData.filmsData.map(item => {console.log(item)})
+const Card =  ({filmsData,timeFilter}) =>{
+    filmsData.map(item => {console.log(item)})
     const convertTime = (time) => {
-
+        const [hr, dummy1, min, dummy2] = time.split(" ")
+        const seconds = (parseInt(hr) * 3600) + (parseInt(min) * 60)
+        return seconds
     }
+    filmsData.map(field => console.log(convertTime(field.duration)))
+    const filteredMovies = filmsData.filter(field =>
+        convertTime(field.duration) > timeFilter.value)
+    console.log(filteredMovies)
     return (
         <>
-        {filmsData.filmsData.map(field => (<li >{field.title} ({field.duration})</li>))}
+        {filteredMovies.map(field => (<li >{field.title} ({convertTime(field.duration)})</li>))}
         </>
         )
 
